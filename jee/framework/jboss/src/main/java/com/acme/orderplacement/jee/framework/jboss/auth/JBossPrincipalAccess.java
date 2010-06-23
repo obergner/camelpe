dab418b6194b6f6e4395626267b13a4d613e76b2
obergner <olaf.bergner@gmx.de>
2010-06-23 23:13:51 +0200

    Ticket #7: Implement a logging interceptor for web service requests and responses  * Prepare merge.

---
+++ jee/framework/jboss/src/main/java/com/acme/orderplacement/jee/framework/jboss/auth/JBossPrincipalAccess.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.acme.orderplacement.jee.framework.jboss.auth;
+
+import java.security.Principal;
+
+import org.jboss.security.SecurityAssociation;
+
+import com.acme.orderplacement.framework.common.auth.PrincipalAccess;
+
+/**
+ * <p>
+ * A <a href="http://www.jboss.org">JBoss</a> specific implementation of
+ * {@link PrincipalAccess <code>PrincipalAccess</code>} that delegates to
+ * JBoss's {@link org.jboss.security.SecurityAssociation
+ * <code>org.jboss.security.SecurityAssociation</code>} when obtaining the
+ * current <code>Principal</code>.
+ * </p>
+ * 
+ * @author <a href="mailto:olaf.bergner@saxsys.de">Olaf Bergner</a>
+ * 
+ */
+public class JBossPrincipalAccess implements PrincipalAccess {
+
+	/**
+	 * @see com.acme.orderplacement.framework.common.auth.PrincipalAccess#currentPrincipal()
+	 */
+	@Override
+	public Principal currentPrincipal() {
+		return SecurityAssociation.getCallerPrincipal();
+	}
+
+}
---
+++ jee/framework/jboss/src/main/java/com/acme/orderplacement/jee/framework/jboss/auth/JaasAuthenticationService.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.acme.orderplacement.jee.framework.jboss.auth;
+
+import javax.security.auth.login.FailedLoginException;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
+import com.acme.orderplacement.jee.framework.common.auth.AuthenticationService;
+
+/**
+ * <p>
+ * An implementation of {@link AuthenticationService
+ * <code>AuthenticationService</code>} that delegates to <tt>Glassfish</tt>'s
+ * {@link ProgrammaticLogin <code>ProgrammaticLogin</code>}.
+ * </p>
+ * 
+ * @author <a href="mailto:olaf.bergner@saxsys.de">Olaf Bergner</a>
+ * 
+ */
+public class JaasAuthenticationService implements AuthenticationService {
+
+	/**
+	 * @see com.acme.orderplacement.jee.framework.common.auth.AuthenticationService#login(java.lang.String,
+	 *      java.lang.String)
+	 */
+	@Override
+	public void login(final String username, final String password)
+			throws FailedLoginException {
+		try {
+			final LoginContext loginContext = new LoginContext(
+					"orderplacement",
+					new ConstantUsernamePasswordCallbackHandler(username,
+							password));
+			loginContext.login();
+		} catch (LoginException e) {
+			throw new FailedLoginException(
+					"Failed to authenticate user identified by username = ["
+							+ username + "] and password = [HIDDEN]: "
+							+ e.getMessage());
+		}
+	}
+
+}
